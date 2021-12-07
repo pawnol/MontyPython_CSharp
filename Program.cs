ba6e9ab0f7d503d2332035ca5507ce1db7f10561
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2021-12-06 20:12:02 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Monty Python
+ * Pawelski
+ * 12/6/2021
+ * Look over the code and predict what the program will 
+ * print in the console. Record your prediction on the
+ * activity sheet. Once you have made your prediction,
+ * check your work by running the program. Lastly, answer
+ * any questions on the activity sheet.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MontyPython
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("********************");
+            Console.WriteLine("*  Monty Python's  *");
+            Console.WriteLine("*  Flying  Circus  *");
+            Console.WriteLine("********************");
+        }
+    }
+}
